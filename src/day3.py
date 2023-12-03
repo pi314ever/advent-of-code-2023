99049e0c7e27de45f3dd29b829255c5c00ff295a
pi314ever <pilotflyer824@gmail.com>
2023-12-03 03:04:25 -0800

    Completed Day 3

---
+++ src/day3.py
@@ -0,0 +1,97 @@
+from utils import get_data_lines, GridNeighborhood
+
+
+class Grid:
+    def __init__(self, data: list[str]):
+        self.data = data
+        self.N, self.M = len(data), len(data[0]) - 1
+        self.neighborhood = GridNeighborhood((self.N, self.M))
+        self.symbol_adj = {}  # (i, j, symbol) -> [nums]
+        self.num_adj = {}  # (i_start, j_start, num) -> [symbols]
+        self.parse()
+
+    def parse(self):
+        for i, line in enumerate(self.data):
+            line = line.strip()
+            j = 0
+            # print(line)
+            while j < len(line):
+                if line[j].isnumeric():
+                    # Find the full number
+                    current_num = get_full_number(self.data, i, j)
+                    # print(current_num)
+                    self._add_number_at(i, j, current_num)
+                    # Skip ahead
+                    j += len(current_num) - 1
+                    # input()
+                j += 1
+
+    def _add_number_at(self, i, j, num):
+        if not self.data[i][j].isnumeric():
+            raise ValueError("The character at the given position is not numeric")
+        # Find the symbols
+        symbols = []
+        symbol_pos = []
+        for i_n, j_n in self.neighborhood.get_block_neighbors(
+            (i, j), (i, j + len(num) - 1), diagonals=True
+        ):
+            if is_symbol(self.data[i_n][j_n]):
+                symbols.append(self.data[i_n][j_n])
+                symbol_pos.append((i_n, j_n))
+        # Add to the dictionary
+        self.num_adj[(i, j, num)] = symbols
+        for symbol, pos in zip(symbols, symbol_pos):
+            if (pos[0], pos[1], symbol) in self.symbol_adj:
+                self.symbol_adj[(pos[0], pos[1], symbol)].append(num)
+            else:
+                self.symbol_adj[(pos[0], pos[1], symbol)] = [num]
+
+
+def main():
+    data = get_data_lines("day3.txt")
+    grid = Grid(data)
+    assert part_1(grid) == 550064
+    assert part_2(grid) == 85010461
+
+
+def part_1(grid: Grid):
+    total = 0
+    for k, v in grid.num_adj.items():
+        if len(v) > 0:
+            total += int(k[2])
+    print("The sum of all part numbers is:", total)
+    return total
+
+
+def part_2(grid: Grid):
+    total = 0
+    for k, v in grid.symbol_adj.items():
+        print(k, v)
+        if k[2] == "*" and len(v) == 2:
+            total += int(v[0]) * int(v[1])
+    print("The sum of all gear ratios is:", total)
+    return total
+
+
+def get_full_number(data: list[str], i, j):
+    # Find the full number
+    current_num = ""
+    for j_n in range(j, -1, -1):
+        if data[i][j_n].isnumeric():
+            current_num = data[i][j_n] + current_num
+        else:
+            break
+    for j_n in range(j + 1, len(data[0])):
+        if data[i][j_n].isnumeric():
+            current_num += data[i][j_n]
+        else:
+            break
+    return current_num
+
+
+def is_symbol(c: str):
+    return not c.isnumeric() and c != "."
+
+
+if __name__ == "__main__":
+    main()
--- src/utils.py
+++ src/utils.py
@@ -11,3 +11,77 @@ def get_data_path(filename):
 def get_data_lines(filename):
     with open(get_data_path(filename)) as f:
         return f.readlines()
+
+
+class GridNeighborhood:
+    def __init__(self, dimensions: tuple[int, int]) -> None:
+        self.N, self.M = dimensions
+
+    def get_neighbors(
+        self, pos: tuple[int, int], diagonals=False
+    ) -> list[tuple[int, int]]:
+        i, j = pos
+        neighbors = []
+        if i > 0:
+            neighbors.append((i - 1, j))
+        if i < self.N - 1:
+            neighbors.append((i + 1, j))
+        if j > 0:
+            neighbors.append((i, j - 1))
+        if j < self.M - 1:
+            neighbors.append((i, j + 1))
+        if diagonals:
+            if i > 0 and j > 0:
+                neighbors.append((i - 1, j - 1))
+            if i > 0 and j < self.M - 1:
+                neighbors.append((i - 1, j + 1))
+            if i < self.N - 1 and j > 0:
+                neighbors.append((i + 1, j - 1))
+            if i < self.N - 1 and j < self.M - 1:
+                neighbors.append((i + 1, j + 1))
+        return neighbors
+
+    def get_block_neighbors(
+        self, pos1: tuple[int, int], pos2: tuple[int, int], diagonals=False
+    ) -> list[tuple[int, int]]:
+        """Get the neighbors of the rectangle defined by pos1 and pos2"""
+        neighbors = []
+        i1, j1 = pos1
+        i2, j2 = pos2
+        i_min, i_max = min(i1, i2), max(i1, i2)
+        j_min, j_max = min(j1, j2), max(j1, j2)
+        # Top edge
+        if i_min > 0:
+            # Top left corner
+            if diagonals and j_min > 0:
+                neighbors.append((i_min - 1, j_min - 1))
+            for j in range(j_min, j_max + 1):
+                neighbors.append((i_min - 1, j))
+            # Top right corner
+            if diagonals and j_max < self.M - 1:
+                neighbors.append((i_min - 1, j_max + 1))
+        # Bottom edge
+        if i_max < self.N - 1:
+            # Bottom left corner
+            if diagonals and j_min > 0:
+                neighbors.append((i_max + 1, j_min - 1))
+            for j in range(j_min, j_max + 1):
+                neighbors.append((i_max + 1, j))
+            # Bottom right corner
+            if diagonals and j_max < self.M - 1:
+                neighbors.append((i_max + 1, j_max + 1))
+        # Left edge
+        if j_min > 0:
+            for i in range(i_min, i_max + 1):
+                neighbors.append((i, j_min - 1))
+        # Right edge
+        if j_max < self.M - 1:
+            for i in range(i_min, i_max + 1):
+                neighbors.append((i, j_max + 1))
+        return neighbors
+
+
+if __name__ == "__main__":
+    # Test neighbors
+    neighborhood = GridNeighborhood((5, 5))
+    print(neighborhood.get_block_neighbors((1, 1), (1, 3), diagonals=True))
