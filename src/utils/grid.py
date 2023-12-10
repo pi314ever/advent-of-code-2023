43056149001551c75728d27dc6cf2015aef01491
pi314ever <pilotflyer824@gmail.com>
2023-12-10 15:30:18 -0800

    Updated grid utils

--- src/utils/grid.py
+++ src/utils/grid.py
@@ -1,18 +1,3 @@
-from .typing import _LenSupport
-
-
-class Grid:
-    """General class for storing a grid of data"""
-
-    def __init__(self, data: list[_LenSupport]) -> None:
-        self.N = len(data)
-        self.M = len(data[0])
-        self.data = data
-
-    def __getitem__(self, key: tuple[int, int]):
-        return self.data[key[0]][key[1]]
-
-
 class GridNeighborhood:
     def __init__(self, dimensions: tuple[int, int]) -> None:
         self.N, self.M = dimensions
@@ -111,3 +96,86 @@ class Direction:
         i, j = pos
         i_delta, j_delta = Direction.COORDINATES[direction]
         return (i + i_delta, j + j_delta)
+
+
+class Grid:
+    """General class for storing a grid of data"""
+
+    def __init__(self, data: list, directions=Direction) -> None:
+        """Initialize grid with given data
+
+        Data is copied to avoid modifying and mutability issues
+        """
+        self._N = len(data)
+        self._M = len(data[0])
+        self.data = [[data[i][j] for j in range(self._M)] for i in range(self._N)]
+        self.neighborhood = GridNeighborhood((self._N, self._M))
+        self.directions = directions
+        self._updated = False
+        self._items = None
+
+    @property
+    def updated(self):
+        return self._updated
+
+    @property
+    def N(self):
+        return self._N
+
+    @property
+    def M(self):
+        return self._M
+
+    @property
+    def items(self):
+        """Lazy evaluated set of items in the grid"""
+        if self._items is None or self._updated:
+            self._items = set(self.to_iter())
+        return self._items
+
+    def __getitem__(self, key: tuple[int, int] | int):
+        if isinstance(key, int):
+            return self.data[key]
+        return self.data[key[0]][key[1]]
+
+    def __setitem__(self, key: tuple[int, int] | int, value):
+        self._updated = True
+        if isinstance(key, int):
+            if len(value) != self._M:
+                raise ValueError("Invalid length")
+            self.data[key] = [v for v in value]
+            return
+        self.data[key[0]][key[1]] = value
+
+    def to_iter(self, column_major=False):
+        """Iterate over the grid, defaulting to row-major order"""
+        if column_major:
+            for j in range(self._M):
+                for i in range(self._N):
+                    yield self.data[i][j]
+        else:
+            for i in range(self._N):
+                for j in range(self._M):
+                    yield self.data[i][j]
+
+    def neighbors(self, pos: tuple[int, int], diagonals=False) -> list[tuple[int, int]]:
+        return self.neighborhood.get_neighbors(pos, diagonals)
+
+    def block_neighbors(
+        self, pos1: tuple[int, int], pos2: tuple[int, int], diagonals=False
+    ) -> list[tuple[int, int]]:
+        return self.neighborhood.get_block_neighbors(pos1, pos2, diagonals)
+
+    def is_valid_pos(self, pos: tuple[int, int]) -> bool:
+        i, j = pos
+        return 0 <= i < self._N and 0 <= j < self._M
+
+    def get_direction(self, src: tuple[int, int], dst: tuple[int, int]) -> str:
+        return self.directions.get_from(src, dst)
+
+    def move_point(self, pos: tuple[int, int], direction: str) -> tuple[int, int]:
+        """Move the point in the given direction, if possible. Otherwise, stay in place."""
+        new_point = self.directions.move(pos, direction)
+        if self.is_valid_pos(new_point):
+            return new_point
+        return pos
