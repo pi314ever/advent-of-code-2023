4d64ea1fe66964bbcc3c1a0051a07eb5e863ae7a
pi314ever <pilotflyer824@gmail.com>
2023-12-30 10:22:33 -0800

    Visual solution for day25

---
+++ src/day25.py
@@ -0,0 +1,62 @@
+from aocd import get_data
+import networkx as nx
+
+import utils
+
+DAY = 25
+
+
+def main():
+    test_data = TEST.splitlines()
+    test_graph = parse(test_data)
+    # assert part_1(test_graph) == 54
+
+    data = get_data(day=DAY, year=utils.YEAR).splitlines()
+    graph = parse(data)
+    part_1(graph)
+
+
+def part_1(graph: nx.Graph):
+    graph.remove_edge("gzr", "qnz")
+    graph.remove_edge("xgs", "lmj")
+    graph.remove_edge("pgz", "hgk")
+    components = nx.connected_components(graph)
+    components = list(components)
+    print(len(components[0]) * len(components[1]))
+    return
+
+
+def visualize(graph: nx.Graph):
+    import matplotlib.pyplot as plt
+
+    nx.draw(graph, with_labels=True)
+    plt.show()
+
+
+def parse(data) -> nx.Graph:
+    graph = nx.Graph()
+    for line in data:
+        node, neighbors = line.split(": ")
+        graph.add_node(node.strip())
+        for n in neighbors.split():
+            graph.add_edge(node.strip(), n.strip())
+    return graph
+
+
+TEST = """jqt: rhn xhk nvd
+rsh: frs pzl lsr
+xhk: hfx
+cmg: qnr nvd lhk bvb
+rhn: xhk bvb hfx
+bvb: xhk hfx
+pzl: lsr hfx nvd
+qnr: nvd
+ntq: jqt hfx bvb xhk
+nvd: lhk
+lsr: lhk
+rzs: qnr cmg lsr rsh
+frs: qnr lhk lsr
+"""
+
+if __name__ == "__main__":
+    main()
